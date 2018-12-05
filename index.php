e59fb11be3215b5b68a627f7db3876aa91e44cb6
moisesmiss <moisesgamiss@gmail.com>
2018-12-04 20:59:19 -0600

    layout basico

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require_once 'controllers/plantilla.controller.php';
+
+$plantilla = new ControllerPlantilla();
+$plantilla->ctrPlantilla();
---
+++ views/modules/header.php
@@ -0,0 +1,53 @@
+<header class="main-header">
+	<!--==============================
+	=            LOGOTIPO            =
+	===============================-->
+	<a href="#" class="logo">
+		<!-- logo mini -->
+		<span class="logo-mini">
+			<i class="fa fa-shopping-bag"></i>
+		</span>
+
+		<span class="logo-lg">
+			<span>INSAE </span>
+			<i class="fa fa-shopping-bag"></i>
+		</span>
+	</a>
+	<!--====  End of LOGOTIPO  ====-->
+
+	<!--=========================================
+	=            BARRA DE NAVEGACION            =
+	==========================================-->
+	<nav class="navbar navbar-static-top">
+		<a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+			
+		</a>
+
+		<!-- PERFIL USUARIO -->
+		<div class="navbar-custom-menu">
+			<ul class="nav navbar-nav">
+				<li class="dropdown user user-menu">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+						<!-- <img src="views/img/usuarios/default/anonymous.png" class="user-image"> -->
+						<i class="fa fa-user"></i>
+						<span class="hidden-xs">Usuario Administrador</span>
+					</a>
+					<ul class="dropdown-menu">
+						<li class="user-body">
+							<div class="pull-right">
+								<a href="#" class="btn btn-default btn-flat">Salir</a>
+							</div>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+
+		
+	</nav>
+	
+	
+	<!--====  End of BARRA DE NAVEGACION  ====-->
+	
+	
+</header>
